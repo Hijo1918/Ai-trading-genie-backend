5dff7815dafc23e21207ed7b383a688a7824f393
Hijo1918 <jonathanije3@gmail.com>
2025-07-13 11:39:17 +0100

    Create app.jsx

---
+++ Src/app.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <main className="p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
