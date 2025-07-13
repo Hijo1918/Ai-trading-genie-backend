6b94f466a813701941e48f365e078ab3cf7dbb6a
Hijo1918 <jonathanije3@gmail.com>
2025-07-13 11:28:37 +0100

    Create navbar.jsx

---
+++ Src/component/navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 text-white p-4 flex justify-between">
+      <h1 className="text-xl font-bold">🚀 MyApp</h1>
+      <div className="space-x-4">
+        <Link to="/" className="hover:underline">Home</Link>
+        <Link to="/about" className="hover:underline">About</Link>
+      </div>
+    </nav>
+  );
+}
